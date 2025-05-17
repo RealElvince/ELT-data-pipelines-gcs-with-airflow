7c100294da90825c1929e9b1a2bcd3eb23a2c977
RealElvince <obuongely@yahoo.com>
2025-05-17 20:39:22 +0300

    added reports_locker

--- dags/reports_locker.py
+++ dags/reports_locker.py
@@ -20,7 +20,7 @@ default_args = {
     'start_date': datetime(2023, 10, 1),
     'retries': 1,
 }
-
+countries = ['UK', 'India', 'Mexico']
 # define the DAG
 with DAG(
     dag_id='split_table_by_country',
@@ -29,3 +29,26 @@ with DAG(
     catchup=False,
     tags=['bigquery', 'partition'],
 ) as dag:
+    
+    # Task to create view for each country-specific table with selected columns and filter
+   for country in countries:
+       create_countries_views = BigqueryInsertJobOperator(
+        task_id=f"create_{country.lower()}_view",
+        configuration={
+            "query": {
+                "query": f"""
+                    CREATE OR REPLACE VIEW `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_view` AS
+                    SELECT
+                      `Year` AS `year`, 
+                            `Disease Name` AS `disease_name`, 
+                            `Disease Category` AS `disease_category`, 
+                            `Prevalence Rate` AS `prevalence_rate`, 
+                            `Incidence Rate` AS `incidence_rate`
+                    FROM
+                      `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table`
+                    WHERE `Availability of Vaccines Treatment` = False
+                """,
+                "useLegacySql": False,
+            }
+        }
+    )
