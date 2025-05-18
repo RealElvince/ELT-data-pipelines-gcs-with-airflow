a8f713257f2e69c37d02688107b99feed8e57a28
RealElvince <obuongely@yahoo.com>
2025-05-18 08:52:11 +0300

    second code refactor on tasks dependencies

--- dags/elt_dag.py
+++ dags/elt_dag.py
@@ -3,16 +3,14 @@ from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQue
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.operators.empty import EmptyOperator
-from  datetime import datetime
+from datetime import datetime
 from dotenv import load_dotenv
 import os
 
-
-# load environment variable
-
+# Load environment variables from .env file
 load_dotenv()
 
-# set default args
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'retries': 1,
@@ -21,44 +19,39 @@ default_args = {
     'email_on_retry': False,
 }
 
-
-# configuration variables
+# Environment variable configurations
 PROJECT_ID = os.getenv('PROJECT_ID')
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 SOURCE_OBJECT = os.getenv('SOURCE_OBJECT')
 DESTINATION_PROJECT_DATASET_TABLE = os.getenv('DESTINATION_PROJECT_DATASET_TABLE')
 BIGQUERY_TABLE = os.getenv('BIGQUERY_TABLE')
-TRANSFORMATION_DATASET= os.getenv('TRANSFORMATION_DATASET')
+TRANSFORMATION_DATASET = os.getenv('TRANSFORMATION_DATASET')
 REPORTS_DATASET = os.getenv('REPORTS_DATASET')
 HEALTH_DATASET = os.getenv('HEALTH_DATASET')
 
-# Country to create seperate tables
-countries =['UK','India','Mexico']
+# List of countries to generate specific tables and views for
+countries = ['UK', 'India', 'Mexico']
 
 # Validate required environment variables
-if not BUCKET_NAME or not SOURCE_OBJECT or not BIGQUERY_TABLE or not DESTINATION_PROJECT_DATASET_TABLE:
-    raise ValueError("Required environment variables (BUCKET_NAME, SOURCE_OBJECT, or DESTINATION_PROJECT_DATASET_TABLE) are missing.")
+required_vars = [BUCKET_NAME, SOURCE_OBJECT, DESTINATION_PROJECT_DATASET_TABLE, BIGQUERY_TABLE]
+if not all(required_vars):
+    raise ValueError("Required environment variables (BUCKET_NAME, SOURCE_OBJECT, BIGQUERY_TABLE, or DESTINATION_PROJECT_DATASET_TABLE) are missing.")
 
-# define the DAG
+# Define the DAG
 with DAG(
     dag_id='ELT_data_pipelines',
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-    start_date = datetime(2023, 10, 1),
+    start_date=datetime(2023, 10, 1),
     tags=['bigquery', 'gcs', 'csv'],
-
 ) as dag:
-    
-    start_task = EmptyOperator(
-        task_id='start_task',
-    )
 
-    end_task = EmptyOperator(
-        task_id='end_task'
-    )
+    # Start and end markers
+    start_task = EmptyOperator(task_id='start_task')
+    end_task = EmptyOperator(task_id='end_task')
 
-    # Task 1: check if the health global data exists in the bucket
+    # Check if file exists in GCS
     check_if_file_exists = GCSObjectExistenceSensor(
         task_id='sensor_to_check_if_csv_file_exists',
         bucket=BUCKET_NAME,
@@ -67,7 +60,8 @@ with DAG(
         timeout=300,
         mode='poke',
     )
-    # Task 2: define the task to load CSV from GCS to Bigquery
+
+    # Load CSV from GCS to BigQuery
     load_csv_to_bq = GCSToBigQueryOperator(
         task_id='load_csv_to_bq',
         bucket=BUCKET_NAME,
@@ -82,60 +76,63 @@ with DAG(
         autodetect=True
     )
 
-    # Task 3: split global health data by country and create tables and views
-    # Task to create table for each country-specific table
-    split_country_data_tasks = []
-    create_countries_views_tasks = []
+   
+
+    # Create table and view tasks for each country
+    split_tasks = []
+    view_tasks = []
     for country in countries:
-       split_task = BigQueryInsertJobOperator(
-        task_id=f"create_table_{country.lower()}",
-        configuration={
-            "query":{
-                "query":f"""
-                          CREATE OR REPLACE TABLE `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table` AS
-                          SELECT *
-                          FROM `{PROJECT_ID}.{HEALTH_DATASET}.{BIGQUERY_TABLE}`
-                          WHERE country='{country}'
-                         """,
-                "useLegacySql":False,
-            }
-        },
-    )
-    # Task to create view for each country-specific table with selected columns and filter
-    view_tasks= BigQueryInsertJobOperator(
-        task_id=f"create_{country.lower()}_view",
-        configuration={
-            "query": {
-                "query": f"""
-                    CREATE OR REPLACE VIEW `{PROJECT_ID}.{REPORTS_DATASET}.{country.lower()}_view` AS
-                    SELECT
-                      `Year` AS `year`, 
+        # Table creation task
+        split_task = BigQueryInsertJobOperator(
+            task_id=f"create_table_{country.lower()}",
+            configuration={
+                "query": {
+                    "query": f"""
+                        CREATE OR REPLACE TABLE `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table` AS
+                        SELECT *
+                        FROM `{PROJECT_ID}.{HEALTH_DATASET}.{BIGQUERY_TABLE}`
+                        WHERE country='{country}'
+                    """,
+                    "useLegacySql": False,
+                }
+            },
+        )
+
+
+        # View creation task
+        view_task = BigQueryInsertJobOperator(
+            task_id=f"create_{country.lower()}_view",
+            configuration={
+                "query": {
+                    "query": f"""
+                        CREATE OR REPLACE VIEW `{PROJECT_ID}.{REPORTS_DATASET}.{country.lower()}_view` AS
+                        SELECT
+                            `Year` AS `year`, 
                             `Disease Name` AS `disease_name`, 
                             `Disease Category` AS `disease_category`, 
                             `Prevalence Rate` AS `prevalence_rate`, 
                             `Incidence Rate` AS `incidence_rate`
-                    FROM
-                      `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table`
-                    WHERE `Availability of Vaccines Treatment` = False
-                """,
-                "useLegacySql": False,
-            }
-        }
-    )
-    
-    # set dependencies for table and view creation
-    split_task.set_upstream(load_csv_to_bq)
-    view_tasks.set_upstream(split_task)
-
-    split_country_data_tasks.append(split_task)
-    create_countries_views_tasks.append(view_tasks)
+                        FROM `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table`
+                        WHERE `Availability of Vaccines Treatment` = False
+                    """,
+                    "useLegacySql": False,
+                }
+            },
+        )
 
     
-    
-    
+       
+        
+        #append tasks to the list
+        split_tasks.append(split_task)
+        view_tasks.append(view_task)
 
-    # define dependencies
+    # Task dependencies
+     # Link initial tasks
     start_task >> check_if_file_exists >> load_csv_to_bq
+    
+    for split_task, view_task in zip(split_tasks, view_tasks):
+        load_csv_to_bq >> split_task >> view_task
+        
 
-    for split_task, view_task in zip(split_country_data_tasks, create_countries_views_tasks):
-       split_task >> view_task>>end_task
+    
