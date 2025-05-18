3914f75d3992d176e48562263a835f89e83efc58
RealElvince <obuongely@yahoo.com>
2025-05-18 16:15:15 +0300

    renamed report file to reports_looker.py

---
+++ dags/reports_looker.py
@@ -0,0 +1,56 @@
+# from airflow import DAG
+# from datetime import datetime
+# from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+# from dotenv import load_dotenv
+# import os
+
+# # load environment variable
+# load_dotenv()
+
+
+# # configuration variables
+# PROJECT_ID = os.getenv('PROJECT_ID')
+# TRANSFORMATION_DATASET = os.getenv('TRANSFORMATION_DATASET')
+# BIGQUERY_TABLE = os.getenv('BIGQUERY_TABLE')
+# REPORTS_DATASET = os.getenv('REPORTS_DATASET')
+
+# # define de
+
+# default_args = {
+#     'start_date': datetime(2023, 10, 1),
+#     'retries': 1,
+# }
+# countries = ['UK', 'India', 'Mexico']
+# # define the DAG
+# with DAG(
+#     dag_id='split_table_by_country',
+#     default_args=default_args,
+#     schedule_interval=None,
+#     catchup=False,
+#     tags=['bigquery', 'partition'],
+# ) as dag:
+    
+#     # Task to create view for each country-specific table with selected columns and filter
+#    create_countries_views = []
+#    for country in countries:
+#        view_tasks = BigQueryInsertJobOperator(
+#         task_id=f"create_{country.lower()}_view",
+#         configuration={
+#             "query": {
+#                 "query": f"""
+#                     CREATE OR REPLACE VIEW `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_view` AS
+#                     SELECT
+#                       `Year` AS `year`, 
+#                             `Disease Name` AS `disease_name`, 
+#                             `Disease Category` AS `disease_category`, 
+#                             `Prevalence Rate` AS `prevalence_rate`, 
+#                             `Incidence Rate` AS `incidence_rate`
+#                     FROM
+#                       `{PROJECT_ID}.{TRANSFORMATION_DATASET}.{country.lower()}_table`
+#                     WHERE `Availability of Vaccines Treatment` = False
+#                 """,
+#                 "useLegacySql": False,
+#             }
+#         }
+#     )
+# create_countries_views.append(view_tasks)
